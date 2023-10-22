da0e44bd8acb33c211b77e6a06cc66fd9780f4ce
romain.pedrero <romain.pedrero@mines-ales.org>
2023-10-22 17:07:21 +0200

    Corrected some mistakes

--- src/main/java/rpo/demo/todo/backend/controller/TodoController.java
+++ src/main/java/rpo/demo/todo/backend/controller/TodoController.java
@@ -4,11 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import rpo.demo.todo.backend.domain.entity.Todo;
 import rpo.demo.todo.backend.service.TodoService;
 
-@RestController("/api/todo/")
+@RequestMapping("/api/todo/")
+@RestController
 public class TodoController {
     private final TodoService todoService;
 
--- src/main/java/rpo/demo/todo/backend/domain/entity/Todo.java
+++ src/main/java/rpo/demo/todo/backend/domain/entity/Todo.java
@@ -3,20 +3,22 @@ package rpo.demo.todo.backend.domain.entity;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import rpo.demo.todo.backend.domain.enums.TodoStatus;
 
 @Getter
 @Setter
+@NoArgsConstructor
 @Entity
 @Table(name = "todo")
 public class Todo {
     @Id
     @GeneratedValue
     private Long id;
-    @NotEmpty
-    @NotBlank
+    @NotNull
     @Enumerated(EnumType.STRING)
     private TodoStatus status;
     @NotEmpty
