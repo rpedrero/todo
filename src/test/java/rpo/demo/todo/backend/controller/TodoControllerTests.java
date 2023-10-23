7f12a5260896b40b21b64daf460a154b2f2f581e
rpedrero <romain.pedrero@gmail.com>
2023-10-23 11:58:13 +0200

    Added REST method for viewing one Todo, and wrote unit test for it. (#7)
    Co-authored-by: romain.pedrero <romain.pedrero[at]mines-ales.org>

--- src/test/java/rpo/demo/todo/backend/controller/TodoControllerTests.java
+++ src/test/java/rpo/demo/todo/backend/controller/TodoControllerTests.java
@@ -7,12 +7,15 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import rpo.demo.todo.backend.entity.Todo;
+import rpo.demo.todo.backend.exception.TodoNotFoundException;
 import rpo.demo.todo.backend.service.TodoService;
 
 import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
@@ -25,53 +28,77 @@ public class TodoControllerTests {
     @InjectMocks
     private TodoController todoController;
 
-    private List<Todo> todos;
+    private Map<Long, Todo> todos;
 
     @BeforeEach
     public void init() {
-        todos = new ArrayList<>();
+        this.todos = new HashMap();
 
         Todo todo1 = new Todo();
         todo1.setId(1L);
         todo1.setTitle("Todo 1");
         todo1.setStatus(true);
-        todos.add(todo1);
+        todos.put(1L, todo1);
 
         Todo todo2 = new Todo();
         todo2.setId(2L);
         todo2.setTitle("Todo 2");
         todo2.setStatus(false);
-        todos.add(todo2);
+        todos.put(2L, todo2);
 
         Todo todo3 = new Todo();
         todo3.setId(3L);
         todo3.setTitle("Todo 3");
         todo3.setStatus(true);
-        todos.add(todo3);
+        todos.put(3L, todo3);
     }
 
     @Test
     public void getAllTest() {
-        when(todoService.getAll()).thenReturn(todos);
+        when(todoService.getAll()).thenReturn(todos.values());
 
         ResponseEntity<Iterable<Todo>> results = this.todoController.getAll();
 
+        Assertions.assertThat(results.getBody()).isNotNull();
+
+        int nbItemsInBody = 0;
         for(Todo todo : results.getBody()) {
-            if(todo.getId() == 1L) {
-                Assertions.assertThat(todo.getTitle()).isEqualTo("Todo 1");
-                Assertions.assertThat(todo.getStatus()).isTrue();
-            }
-            else if(todo.getId() == 2L) {
-                Assertions.assertThat(todo.getTitle()).isEqualTo("Todo 2");
-                Assertions.assertThat(todo.getStatus()).isFalse();
-            }
-            else if(todo.getId() == 3L) {
-                Assertions.assertThat(todo.getTitle()).isEqualTo("Todo 3");
-                Assertions.assertThat(todo.getStatus()).isTrue();
-            } else {
-                Assertions.fail("At least one item should not be part of the response.");
-            }
+            Assertions.assertThat(todo.getTitle()).isEqualTo(this.todos.get(todo.getId()).getTitle());
+            Assertions.assertThat(todo.getStatus()).isEqualTo(this.todos.get(todo.getId()).getStatus());
+
+            nbItemsInBody++;
         }
+        Assertions.assertThat(nbItemsInBody).isEqualTo(this.todos.size());
+    }
+
+    @Test
+    public void getTest() {
+        when(this.todoService.getTodo(any(Long.class))).thenAnswer(invocation -> {
+            Long argument = (Long) invocation.getArguments()[0];
+
+            if(this.todos.containsKey(argument)) {
+                return this.todos.get(argument);
+            }
+            else {
+                throw new TodoNotFoundException();
+            }
+        });
+
+        for(long i = 1L; i <= 3L; i++) {
+            ResponseEntity<Todo> response = this.todoController.get(i);
+
+            Assertions.assertThat(response.getBody()).isNotNull();
+
+            Todo todo = response.getBody();
+            Assertions.assertThat(todo.getId()).isEqualTo(i);
+            Assertions.assertThat(todo.getTitle()).isEqualTo(this.todos.get(i).getTitle());
+            Assertions.assertThat(todo.getStatus()).isEqualTo(this.todos.get(i).getStatus());
+        }
+
+        ResponseEntity<Todo> responseForUnexistingTodoId = this.todoController.get(4L);
+
+        Assertions.assertThat(responseForUnexistingTodoId.getBody()).isNull();
+        Assertions.assertThat(responseForUnexistingTodoId.getStatusCode()).isEqualTo(HttpStatusCode.valueOf(404));
     }
 
     @Test
@@ -79,13 +106,22 @@ public class TodoControllerTests {
         Todo todo = new Todo();
         todo.setStatus(true);
 
-        when(this.todoService.getTodo(any(Long.class))).thenReturn(this.todos.get(1));
+        when(this.todoService.getTodo(any(Long.class))).thenAnswer(invocation -> {
+            Long id = (Long) invocation.getArguments()[0];
+
+            if(this.todos.containsKey(id)) {
+                return this.todos.get(id);
+            }
+            else {
+                throw new TodoNotFoundException();
+            }
+        });
 
         when(this.todoService.updateTodo(any(Todo.class))).thenAnswer(invocation -> {
             Object[] args = invocation.getArguments();
             Todo arg = ((Todo) args[0]);
 
-            Todo entityToModify = this.todos.get(1);
+            Todo entityToModify = this.todos.get(arg.getId());
             entityToModify.setStatus(arg.getStatus());
             entityToModify.setTitle(arg.getTitle());
 
